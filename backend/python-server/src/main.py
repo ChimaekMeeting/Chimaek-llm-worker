c8c84cf8da6085d6e39e6c203f69a219df7c4ba3
202310957 <snu11025@naver.com>
2026-02-11 10:30:36 +0900

    build: Dockerfile, docker-compose.yml 추가

--- backend/python-server/src/main.py
+++ backend/python-server/src/main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from src.entity.base import init_db
 from src.api import user, recommendation
-import uvicorn
+# import uvicorn
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -35,5 +35,5 @@ app.include_router(recommendation.router)
 def read_root():
     return {"message": "산책 경로 추천 서비스입니다."}
 
-if __name__ == "__main__":
-    uvicorn.run("src.main:app", host="127.0.0.1", port=8080, reload=True)
+# if __name__ == "__main__":
+#     uvicorn.run("src.main:app", host="127.0.0.1", port=8080, reload=True)
